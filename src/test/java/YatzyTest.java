42518f5ca6ce36842a6185aac91e891b919e941e
arek-dabrowski <arekdbr97@gmail.com>
2020-01-22 18:37:52 +0100

    some refactors

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -62,30 +62,30 @@ public class YatzyTest {
 
     @Test
     public void one_pair() {
-        assertEquals(6, Yatzy.score_pair(3,4,3,5,6));
-        assertEquals(10, Yatzy.score_pair(5,3,3,3,5));
-        assertEquals(12, Yatzy.score_pair(5,3,6,6,5));
+        assertEquals(6, Yatzy.scorePair(3,4,3,5,6));
+        assertEquals(10, Yatzy.scorePair(5,3,3,3,5));
+        assertEquals(12, Yatzy.scorePair(5,3,6,6,5));
     }
 
     @Test
     public void two_Pair() {
-        assertEquals(16, Yatzy.two_pair(3,3,5,4,5));
-        assertEquals(16, Yatzy.two_pair(3,3,5,5,5));
+        assertEquals(16, Yatzy.twoPair(3,3,5,4,5));
+        assertEquals(16, Yatzy.twoPair(3,3,5,5,5));
     }
 
     @Test
     public void three_of_a_kind() 
     {
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,4,5));
-        assertEquals(15, Yatzy.three_of_a_kind(5,3,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,5));
+        assertEquals(9, Yatzy.threeOfAKind(3,3,3,4,5));
+        assertEquals(15, Yatzy.threeOfAKind(5,3,5,4,5));
+        assertEquals(9, Yatzy.threeOfAKind(3,3,3,3,5));
     }
 
     @Test
     public void four_of_a_knd() {
-        assertEquals(12, Yatzy.four_of_a_kind(3,3,3,3,5));
-        assertEquals(20, Yatzy.four_of_a_kind(5,5,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,3));
+        assertEquals(12, Yatzy.fourOfAKind(3,3,3,3,5));
+        assertEquals(20, Yatzy.fourOfAKind(5,5,5,4,5));
+        assertEquals(9, Yatzy.threeOfAKind(3,3,3,3,3));
     }
 
     @Test
