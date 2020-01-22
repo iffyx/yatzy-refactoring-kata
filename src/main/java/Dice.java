99d67345a3d3b032a8ab331f552c8ddbd179eefd
arek-dabrowski <arekdbr97@gmail.com>
2020-01-22 20:41:57 +0100

    refactors part 5

---
+++ src/main/java/Dice.java
@@ -0,0 +1,20 @@
+public class Dice {
+    private int rollResult;
+
+    public Dice(int numberToRoll) {
+        rollResult = numberToRoll;
+    }
+
+    public int getRollResult() {
+        return rollResult;
+    }
+
+    public void setRollResult(int rollResult) {
+        this.rollResult = rollResult;
+    }
+
+    public void throwDice() {
+        // TODO: Random dice throw.
+    }
+}
+
--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -1,37 +1,34 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class Yatzy {
 
-    public static final int YATZY_SCORE = 50;
+    private static final int YATZY_SCORE = 50;
+    private static final int NUMBER_OF_DICES = 5;
 
-    protected List<Integer> dicesResults;
-
-    public Yatzy(List<Integer> dicesResults) {
-        this.dicesResults = dicesResults;
+    public Yatzy() {
     }
 
-    public int chance(List<Integer> dicesResults) {
+    public int chance(List<Dice> dicesResults) {
         int sum = 0;
-        for (Integer result : dicesResults) {
-            sum += result;
+        for (Dice result : dicesResults) {
+            sum += result.getRollResult();
         }
         return sum;
     }
 
-    public int yatzy(List<Integer> dicesResults) {
+    public int yatzy(List<Dice> dicesResults) {
         int[] tallies = getTallies(dicesResults);
-        for (int i = 5; i >= 0; i--)
+        for (int i = NUMBER_OF_DICES; i >= 0; i--)
             if (tallies[i] == 5)
                 return YATZY_SCORE;
         return 0;
     }
 
-    public int twoPair(List<Integer> dicesResults) {
+    public int twoPair(List<Dice> dicesResults) {
         int[] tallies = getTallies(dicesResults);
         int numberOfPairs = 0;
         int score = 0;
-        for (int i = 5; i >= 0; i--)
+        for (int i = NUMBER_OF_DICES; i >= 0; i--)
             if (tallies[i] >= 2) {
                 numberOfPairs++;
                 score += (i + 1);
@@ -42,37 +39,37 @@ public class Yatzy {
             return 0;
     }
 
-    public int sumsOfAKind(List<Integer> dicesResults, int kindNumber) {
+    public int sumOfAKind(List<Dice> dicesResults, int kindNumber) {
         int sum = 0;
-        for (Integer result : dicesResults) {
-            if (result == kindNumber) sum += kindNumber;
+        for (Dice result : dicesResults) {
+            if (result.getRollResult() == kindNumber) sum += kindNumber;
         }
         return sum;
     }
 
-    public int numberOfAKind(List<Integer> dicesResults, int kindNumber) {
+    public int numberOfAKind(List<Dice> dicesResults, int kindNumber) {
         int[] tallies = getTallies(dicesResults);
-        for (int i = 5; i >= 0; i--)
+        for (int i = NUMBER_OF_DICES; i >= 0; i--)
             if (tallies[i] >= kindNumber)
                 return (i + 1) * kindNumber;
         return 0;
     }
 
-    public int straight(List<Integer> dicesResults, int highestDiceResult) {
+    public int straight(List<Dice> dicesResults, int highestDiceResult) {
         int[] tallies = getTallies(dicesResults);
         if (isStraight(tallies, highestDiceResult))
             return (highestDiceResult - 2) * 5;
         return 0;
     }
-    
-    public int fullHouse(List<Integer> dicesResults) {
+
+    public int fullHouse(List<Dice> dicesResults) {
         int[] tallies = getTallies(dicesResults);
         boolean pairOccurs, threeOccurs;
         pairOccurs = threeOccurs = false;
         int pairDiceValue, threeDiceValue;
         pairDiceValue = threeDiceValue = 0;
 
-        for (int i = 5; i >= 0; i--) {
+        for (int i = NUMBER_OF_DICES; i >= 0; i--) {
             if (tallies[i] == 2) {
                 pairOccurs = true;
                 pairDiceValue = i + 1;
@@ -88,15 +85,15 @@ public class Yatzy {
         return 0;
     }
 
-    private int[] getTallies(List<Integer> dicesResults) {
+    private int[] getTallies(List<Dice> dicesResults) {
         int[] tallies = new int[6];
-        for (Integer result : dicesResults) {
-            tallies[result - 1]++;
+        for (Dice result : dicesResults) {
+            tallies[result.getRollResult() - 1]++;
         }
         return tallies;
     }
 
-    private boolean isStraight(int[] tallies, int highestDiceResult){
+    private boolean isStraight(int[] tallies, int highestDiceResult) {
         return tallies[highestDiceResult] == 1 &&
                 tallies[highestDiceResult - 1] == 1 &&
                 tallies[highestDiceResult - 2] == 1 &&
@@ -104,6 +101,3 @@ public class Yatzy {
                 tallies[highestDiceResult - 4] == 1;
     }
 }
-
-
-
